d2eadde096af2af76d541c9c5bb8b78eb80f5c0c
OskarPodgorski <oskar.podgorski.97@gmail.com>
2025-01-28 15:28:39 +0100

    added animations to buttons

---
+++ BulkSMSSender2.0/Libraries/ClickAnimation.cs
@@ -0,0 +1,25 @@
+﻿namespace BulkSMSSender2._0;
+
+public class ClickAnimation : Behavior<Button>
+{
+    protected override void OnAttachedTo(Button button)
+    {
+        base.OnAttachedTo(button);
+        button.Clicked += OnButtonClicked;
+    }
+
+    protected override void OnDetachingFrom(Button button)
+    {
+        base.OnDetachingFrom(button);
+        button.Clicked -= OnButtonClicked;
+    }
+
+    private async void OnButtonClicked(object sender, EventArgs e)
+    {
+        if (sender is Button button)
+        {
+            await button.ScaleTo(0.88, 35);
+            await button.ScaleTo(1, 35);
+        }
+    }
+}
